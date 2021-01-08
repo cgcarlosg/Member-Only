cac1e8f8682bfa5cd7cc4c3244e64295ce176527
Carlos-SSH <carlos.anezeh@me.com>
2021-01-08 14:14:54 -0600

    Fixed corrections with linter.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,69 +1,67 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: %i[show edit update destroy]
-    before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: %i[edit update destroy]
 
-    def index
-      @posts = Post.all.order('created_at DESC')
-      @post = Post.new
-    end
-  
-    def show
-      @post = set_post
-    end
-  
-    def new
-      @post = current_user.posts.build
-    end
+  def index
+    @posts = Post.all.order('created_at DESC')
+    @post = Post.new
+  end
 
-    def edit
-    end
-  
-    def create
-      @post = current_user.posts.build(post_params)
-  
-      respond_to do |format|
-        if @post.save
-          format.html { redirect_to root_path, notice: 'Post was successfully created.' }
-          format.json { render :show, status: :created, location: @post }
-  
-        else
-          format.html { render :new }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-  
-        end
+  def show
+    @post = set_post
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def edit; end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+
       end
     end
+  end
 
-    def update
-      respond_to do |format|
-        if @post.update(post_params)
-          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-          format.json { render :show, status: :ok, location: @post }
-  
-        else
-          format.html { render :edit }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-  
-        end
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+
       end
     end
-  
-    def destroy
-      @post.destroy
-      respond_to do |format|
-        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    end
-  
-    private
+  end
 
-    def set_post
-      @post = Post.find(params[:id])
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
     end
+  end
 
+  private
 
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-    validates_length_of :title, :minimum => 5, :allow_nil => false
-    validates_length_of :body, :minimum => 7, :allow_nil => false
-    
-    belongs_to :user
+  validates_length_of :title, minimum: 5, allow_nil: false
+  validates_length_of :body, minimum: 7, allow_nil: false
+
+  belongs_to :user
 end
