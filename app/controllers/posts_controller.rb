18fb776e297c5864cf248e10039c00ebf45c69d3
cgcarlosg <cgcarlosg@hotmail.com>
2021-01-08 12:02:54 -0500

    fix databases migrations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,79 +1,66 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: %i[index show]
+    before_action :set_post, only: %i[show edit update destroy]
+    before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
-  # GET /tweeets
-  # GET /tweeets.json
-  def index
-    @posts = Post.all.order('created_at DESC')
-    @post = Post.new
-  end
+    def index
+      @posts = Post.all.order('created_at DESC')
+      @post = Post.new
+    end
+  
+    def show; end
+  
+    def new
+      @post = current_user.posts.build
+    end
 
-  # GET /tweeets/1
-  # GET /tweeets/1.json
-  def show; end
-
-  # GET /tweeets/new
-  def new
-    @post = current_user.posts.build
-  end
-
-  # GET /tweeets/1/edit
-  def edit; end
-
-  # POST /tweeets
-  # POST /tweeets.json
-  def create
-    @post = current_user.posts.build(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-
+    def edit; end
+  
+    def create
+      @post = current_user.posts.build(post_params)
+  
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+          format.json { render :show, status: :created, location: @post }
+  
+        else
+          format.html { render :new }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+  
+        end
       end
     end
-  end
 
-  # PATCH/PUT /tweeets/1
-  # PATCH/PUT /tweeets/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-
+    def update
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+          format.json { render :show, status: :ok, location: @post }
+  
+        else
+          format.html { render :edit }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+  
+        end
       end
     end
-  end
-
-  # DELETE /tweeets/1
-  # DELETE /tweeets/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+  
+    def destroy
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
-  end
+  
+    private
 
-  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
-    @post = Post.find(params[:id])
-  end
 
-  # Only allow a list of trusted parameters through.
-  def post_params
-    params.require(:post).permit(:post)
-  end
+    def post_params
+      params.require(:post).permit(:post)
+    end
 end
