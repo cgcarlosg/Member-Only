a56197fa869a5a49482403b41f2b174b34ca5a87
cgcarlosg <cgcarlosg@hotmail.com>
2021-01-08 11:11:15 -0500

    fix migrations in tables

--- db/migrate/20210106225919_user_id_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class UserIdToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :user_ids, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_152203) do
+ActiveRecord::Schema.define(version: 2021_01_08_160752) do
 
   create_table "posts", force: :cascade do |t|
-    t.text "post"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-  end
-
-  create_table "tweeets", force: :cascade do |t|
-    t.text "tweeet"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
