3702dc19cb55077a439c6a1214fc382513a9df1d
Carlos-SSH <carlos.anezeh@me.com>
2021-01-09 07:36:34 -0600

    Fixed issues in webpacker and fixed the logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   resources :posts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
 end
